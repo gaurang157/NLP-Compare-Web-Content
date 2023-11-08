e17c9cec9ba2f32316db7e877f50e25629c66248
gaurang157 <148379526+gaurang157@users.noreply.github.com>
2023-11-08 13:02:01 +0530

    Update 2_NLP_Compare_Web_Content.py

--- 2_NLP_Compare_Web_Content.py
+++ 2_NLP_Compare_Web_Content.py
@@ -23,16 +23,16 @@ st.write("Enter the URLs of Articles to process:")
 '''e.g. News Aticle, Wiki, etc'''
 # st.sidebar.markdown("Process any Text🔡Web🕸️Page📄")
  #✅🎑🎉🎯
-st.sidebar.link_button("NLP Web Content Analysis", "https://streamlit.io/gallery",use_container_width=True)
+st.sidebar.link_button("NLP Web Content Analysis", "https://nlp-web-content-analysis.streamlit.app/",use_container_width=True)
 
 # st.sidebar.markdown("Compare any Text🔡Web🕸️Page📄")
-st.sidebar.link_button("NLP Compare Web Content (Selected 🎉 ✅)", "https://streamlit.io/gallery",use_container_width=True)
+st.sidebar.link_button("NLP Compare Web Content (Selected 🎉 ✅)", "https://nlp-compare-web-content.streamlit.app/",use_container_width=True)
 
 # st.sidebar.markdown("Process any YouTube ▶️ Video of English Language")
-st.sidebar.link_button("NLP YouTube Analysis", "https://streamlit.io/gallery",use_container_width=True)
+st.sidebar.link_button("NLP YouTube Analysis", "https://nlp-youtube-analysis.streamlit.app/",use_container_width=True)
 
 # st.sidebar.markdown("Compare any YT ▶️ with captions")
-st.sidebar.link_button("NLP Compare YouTube Videos", "https://streamlit.io/gallery",use_container_width=True)
+st.sidebar.link_button("NLP Compare YouTube Videos", "https://nlp-compare-youtube-videos.streamlit.app/",use_container_width=True)
 col1, col2 = st.columns(2)
 with col1:
     
